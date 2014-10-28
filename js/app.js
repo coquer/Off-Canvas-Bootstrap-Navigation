70d64f314b06a52429330f517304ad1d626dd058
jycr753 <jycr753@gmail.com>
2014-10-28 13:08:49 +0100

    Merge branch 'master' of https://github.com/jycr753/Sliding-Menu-CSS
    Conflicts:
    	image.png

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
-	$("#nav-trigger").on("click", function(event){
-
-		console.log("Clicking");
+document.addEventListener('DOMContentLoaded', function(){
+	var nav_trigger = document.getElementById("nav-trigger");
+	nav_trigger.addEventListener("click", function(){
+		var col_nav = document.getElementById("col-nav"),
+		active_class = "col-nav-active";
+		if (nav_trigger.checked){
+			col_nav.classList.add(active_class);
+		}else{
+			col_nav.classList.remove(active_class);
+		}
 	});
-	
 });
